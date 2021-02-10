7bd093cc0b9dc02ad374161a091b94c7fee49a01
Jos van der Zande <jvdzande@yahoo.com>
2021-02-10 15:45:31 +0100

    Added second host support for ximmio.

--- garbagecalendar/m_ximmio.lua
+++ garbagecalendar/m_ximmio.lua
@@ -1,9 +1,9 @@
 -----------------------------------------------------------------------------------------------------------------
 -- garbagecalendar module script: m_ximmio.lua
 ----------------------------------------------------------------------------------------------------------------
-ver="20201217-1500"
+ver="20210210-1540"
 websitemodule="m_ximmio"
--- API WebSite:  https://wasteapi.2go-mobile.com/api
+-- API WebSite:  https://wasteapi.2go-mobile.com/api  &  https://wasteprod2api.ximmio.com
 --
 --- Find your companycode by:
 --   1. goto your webpage calendar and display the garbage calendar
@@ -72,9 +72,10 @@ function Perform_Update()
    end
    dprint('---- web update ----------------------------------------------------------------------------')
    local Web_Data
+   local webhost = "https://wasteprod2api.ximmio.com"
    ---
    -- Get the information for the specified address specifically the UniqueId for the subsequent calls
-   Web_Data=perform_webquery('--data "companyCode='..companyCode..'&postCode='..Zipcode..'&houseNumber='..Housenr.."&houseNumberAddition="..Housenrsuf..'" "https://wasteapi.2go-mobile.com/api/FetchAdress"')
+   Web_Data=perform_webquery('--data "companyCode='..companyCode..'&postCode='..Zipcode..'&houseNumber='..Housenr.."&houseNumberAddition="..Housenrsuf..'" "' .. webhost.. '/api/FetchAdress"')
    if Web_Data == "" then
       return
    end
@@ -83,17 +84,27 @@ function Perform_Update()
       return
    end
    adressdata = JSON:decode(Web_Data)
-    -- Decode JSON table and find the appropriate address when there are multiple options when toevoeging is used like 10a
+   -- Decode JSON table and find the appropriate address when there are multiple options when toevoeging is used like 10a
+   if adressdata['dataList'] == nil or adressdata['dataList'][1] == nil then
+      webhost = "https://wasteapi.2go-mobile.com"
+      Web_Data=perform_webquery('--data "companyCode='..companyCode..'&postCode='..Zipcode..'&houseNumber='..Housenr.."&houseNumberAddition="..Housenrsuf..'" "' .. webhost.. '/api/FetchAdress"')
+      adressdata = JSON:decode(Web_Data)
+   end
+   if adressdata['dataList'] == nil or adressdata['dataList'][1] == nil then
+      dprint("### Error: No UniqueId retrieved, datalist missing...  stopping execution.")
+      return
+   end
    UniqueId = adressdata['dataList'][1]['UniqueId']
    if UniqueId == nil or UniqueId == "" then
       dprint("### Error: No UniqueId retrieved...  stopping execution.")
       return
    end
+
    dprint("UniqueId:"..UniqueId)
    -- set startdate to today en end date to today + 28 days
    startDate=os.date("%Y-%m-%d")
    endDate=os.date("%Y-%m-%d",os.time()+28*24*60*60)
-   Web_Data=perform_webquery('--data "companyCode='..companyCode..'&uniqueAddressID='..UniqueId..'&startDate='..startDate.."&endDate="..endDate..'" "https://wasteapi.2go-mobile.com/api/GetCalendar"')
+   Web_Data=perform_webquery('--data "companyCode='..companyCode..'&uniqueAddressID='..UniqueId..'&startDate='..startDate.."&endDate="..endDate..'" "' .. webhost.. '/api/GetCalendar"')
    if ( Web_Data:sub(1,2) == "[]" ) then
       dprint("### Error: Unable to retrieve Afvalstromen information...  stopping execution.")
       return
